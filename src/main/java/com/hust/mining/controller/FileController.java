0c7448da8804faaf5057aa4bf6e79d061987fcf6
zhang <zhang@USER-20160621IY>
2016-12-22 15:23:07 +0800

    修改多文件上传中：假如第一个文件为空，后面文件不能上传的问题。

--- src/main/java/com/hust/mining/controller/FileController.java
+++ src/main/java/com/hust/mining/controller/FileController.java
@@ -2,6 +2,7 @@ package com.hust.mining.controller;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -36,169 +37,174 @@ import net.sf.json.JSONObject;
 @Controller
 @RequestMapping("/file")
 public class FileController {
-    /**
-     * Logger for this class
-     */
-    private static final Logger logger = LoggerFactory.getLogger(FileController.class);
+	/**
+	 * Logger for this class
+	 */
+	private static final Logger logger = LoggerFactory.getLogger(FileController.class);
 
-    @Autowired
-    private FileService fileService;
-    @Autowired
-    private IssueService issueService;
-    @Autowired
-    private ResultService resultService;
+	@Autowired
+	private FileService fileService;
+	@Autowired
+	private IssueService issueService;
+	@Autowired
+	private ResultService resultService;
 
-    @ResponseBody
-    @RequestMapping(value = "/upload", method = RequestMethod.POST)
-    public Object upload(@RequestParam(value = "file", required = true) MultipartFile[] file,
-            @RequestParam(value = "titleIndex", required = true) int[] titleIndex,
-            @RequestParam(value = "timeIndex", required = true) int[] timeIndex,
-            @RequestParam(value = "urlIndex", required = true) int[] urlIndex,
-            @RequestParam(value = "sourceType", required = true) String[] sourceType, HttpServletRequest request) {
-        if (request.getSession().getAttribute(KEY.ISSUE_ID) == null) {
-            return ResultUtil.errorWithMsg("请选择或者创建一个话题");
-        }
-        // 数组之间必须是一一对应关系
-        for (int i = 0; i < file.length; i++) {
-            if (file[i].isEmpty()) {
-                return ResultUtil.errorWithMsg("文件为空");
-            }
-            Condition condition = new Condition();
-            condition.setFile(file[i]);
-            condition.setTimeIndex(timeIndex[i]);
-            condition.setUrlIndex(urlIndex[i]);
-            condition.setTitleIndex(titleIndex[i]);
-            condition.setSourceType(sourceType[i]);
-            if (fileService.insert(condition, request) == 0) {
-                return ResultUtil.errorWithMsg("上传失败");
-            }
-        }
-        return ResultUtil.success("上传成功");
-    }
+	@ResponseBody
+	@RequestMapping(value = "/upload", method = RequestMethod.POST)
+	public Object upload(@RequestParam(value = "file", required = true) MultipartFile[] file,
+			@RequestParam(value = "titleIndex", required = true) int[] titleIndex,
+			@RequestParam(value = "timeIndex", required = true) int[] timeIndex,
+			@RequestParam(value = "urlIndex", required = true) int[] urlIndex,
+			@RequestParam(value = "sourceType", required = true) String[] sourceType, HttpServletRequest request) {
+		if (request.getSession().getAttribute(KEY.ISSUE_ID) == null) {
+			return ResultUtil.errorWithMsg("请选择或者创建一个话题");
+		}
+		// 数组之间必须是一一对应关系
+		List<String> info = new ArrayList<>();
+		for (int i = 0; i < file.length; i++) {
+			if (file[i].isEmpty()) {
+				logger.info(file[i] + "is empty");
+				info.add(file[i] + "is empty");
+				continue;
+			}
+			Condition condition = new Condition();
+			condition.setFile(file[i]);
+			condition.setTimeIndex(timeIndex[i]);
+			condition.setUrlIndex(urlIndex[i]);
+			condition.setTitleIndex(titleIndex[i]);
+			condition.setSourceType(sourceType[i]);
+			if (fileService.insert(condition, request) == 0) {
+				logger.info(file[i] + "is empty");
+				info.add(file[i] + "is empty");
+				continue;
+			}
+		}
+		return ResultUtil.success(info);
+	}
 
-    // @SuppressWarnings("unchecked")
-    // @RequestMapping("/download")
-    // public void download(HttpServletRequest request, HttpServletResponse
-    // response) throws IOException {
-    // Object uuidObj = request.getSession().getAttribute(KEY.ISSUE_ID);
-    // String uuid = uuidObj == null ? StringUtils.EMPTY : uuidObj.toString();
-    // if (StringUtils.isBlank(uuid)) {
-    // response.sendError(404, "未找到当前处理事件，请先创建或者选择某一事件");
-    // logger.info("从session中无法获得文件uuid");
-    // return;
-    // }
-    // OutputStream outputStream = null;
-    // try {
-    // Issue issue = issueService.queryIssueWithBLOBsById(uuid);
-    // List<String[]> relist = (List<String[]>)
-    // ConvertUtil.convertBytesToObject(issue.getClusterResult());
-    // List<String[]> origlist = (List<String[]>)
-    // ConvertUtil.convertBytesToObject(issue.getOrigCountResult());
-    // outputStream = response.getOutputStream();
-    // response.setCharacterEncoding("utf-8");
-    // response.setContentType("multipart/form-data");
-    // response.setHeader("Content-Disposition",
-    // "attachment;fileName=result.xls");
-    // HSSFWorkbook workbook = ExcelUtil.exportToExcel(relist, origlist);
-    // workbook.write(outputStream);
-    // } catch (Exception e) {
-    // logger.info("excel 导出失败\t" + e.toString());
-    // } finally {
-    // try {
-    // outputStream.close();
-    // } catch (IOException e) {
-    // logger.info("导出excel时，关闭outputstream失败");
-    // }
-    // }
-    // }
-    @SuppressWarnings("unchecked")
-    @RequestMapping("/download")
-    public void download(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String issueId = issueService.getCurrentIssueId(request);
-        if (StringUtils.isBlank(issueId)) {
-            response.sendError(404, "未找到当前处理事件，请先创建或者选择某一事件");
-            logger.info("从session中无法s获得话题的话题id");
-            return;
-        }
-        String resultId = resultService.getCurrentResultId(request);
-        if (StringUtils.isBlank(resultId)) {
-            response.sendError(404, "未找到当前处理记录，请先创建或者选择某一记录");
-            logger.info("从session中无法s获得记录的记录id");
-            return;
-        }
-        OutputStream outputStream = null;
-        try {
-            Map<String, List<String[]>> map = resultService.exportService(issueId, resultId);
-            if (map == null) {
-                response.sendError(404, "导出错误");
-                return;
-            }
-            List<String[]> cluster = map.get("cluster");
-            List<String[]> count = map.get("count");
-            outputStream = response.getOutputStream();
-            response.setCharacterEncoding("utf-8");
-            response.setContentType("multipart/form-data");
-            response.setHeader("Content-Disposition", "attachment;fileName=result.xls");
-            HSSFWorkbook workbook = ExcelUtil.exportToExcel(cluster, count);
-            workbook.write(outputStream);
-        } catch (Exception e) {
-            logger.info("excel 导出失败\t" + e.toString());
-        } finally {
-            try {
-                outputStream.close();
-            } catch (IOException e) {
-                logger.info("导出excel时，关闭outputstream失败");
-            }
-        }
-    }
+	// @SuppressWarnings("unchecked")
+	// @RequestMapping("/download")
+	// public void download(HttpServletRequest request, HttpServletResponse
+	// response) throws IOException {
+	// Object uuidObj = request.getSession().getAttribute(KEY.ISSUE_ID);
+	// String uuid = uuidObj == null ? StringUtils.EMPTY : uuidObj.toString();
+	// if (StringUtils.isBlank(uuid)) {
+	// response.sendError(404, "未找到当前处理事件，请先创建或者选择某一事件");
+	// logger.info("从session中无法获得文件uuid");
+	// return;
+	// }
+	// OutputStream outputStream = null;
+	// try {
+	// Issue issue = issueService.queryIssueWithBLOBsById(uuid);
+	// List<String[]> relist = (List<String[]>)
+	// ConvertUtil.convertBytesToObject(issue.getClusterResult());
+	// List<String[]> origlist = (List<String[]>)
+	// ConvertUtil.convertBytesToObject(issue.getOrigCountResult());
+	// outputStream = response.getOutputStream();
+	// response.setCharacterEncoding("utf-8");
+	// response.setContentType("multipart/form-data");
+	// response.setHeader("Content-Disposition",
+	// "attachment;fileName=result.xls");
+	// HSSFWorkbook workbook = ExcelUtil.exportToExcel(relist, origlist);
+	// workbook.write(outputStream);
+	// } catch (Exception e) {
+	// logger.info("excel 导出失败\t" + e.toString());
+	// } finally {
+	// try {
+	// outputStream.close();
+	// } catch (IOException e) {
+	// logger.info("导出excel时，关闭outputstream失败");
+	// }
+	// }
+	// }
+	@SuppressWarnings("unchecked")
+	@RequestMapping("/download")
+	public void download(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		String issueId = issueService.getCurrentIssueId(request);
+		if (StringUtils.isBlank(issueId)) {
+			response.sendError(404, "未找到当前处理事件，请先创建或者选择某一事件");
+			logger.info("从session中无法s获得话题的话题id");
+			return;
+		}
+		String resultId = resultService.getCurrentResultId(request);
+		if (StringUtils.isBlank(resultId)) {
+			response.sendError(404, "未找到当前处理记录，请先创建或者选择某一记录");
+			logger.info("从session中无法s获得记录的记录id");
+			return;
+		}
+		OutputStream outputStream = null;
+		try {
+			Map<String, List<String[]>> map = resultService.exportService(issueId, resultId);
+			if (map == null) {
+				response.sendError(404, "导出错误");
+				return;
+			}
+			List<String[]> cluster = map.get("cluster");
+			List<String[]> count = map.get("count");
+			outputStream = response.getOutputStream();
+			response.setCharacterEncoding("utf-8");
+			response.setContentType("multipart/form-data");
+			response.setHeader("Content-Disposition", "attachment;fileName=result.xls");
+			HSSFWorkbook workbook = ExcelUtil.exportToExcel(cluster, count);
+			workbook.write(outputStream);
+		} catch (Exception e) {
+			logger.info("excel 导出失败\t" + e.toString());
+		} finally {
+			try {
+				outputStream.close();
+			} catch (IOException e) {
+				logger.info("导出excel时，关闭outputstream失败");
+			}
+		}
+	}
 
-    @ResponseBody
-    @RequestMapping(value = "/queryIssueFiles")
-    public Object queryIssueFiles(@RequestParam(value = "issueId", required = true) String issueId,
-            HttpServletRequest request) {
-        String user = request.getSession().getAttribute(KEY.USER_NAME).toString();
-        IssueQueryCondition con = new IssueQueryCondition();
-        con.setIssueId(issueId);
-        con.setUser(user);
-        List<Issue> issues = issueService.queryIssue(con);
-        if (issues.isEmpty()) {
-            return ResultUtil.errorWithMsg("查询话题文件失败");
-        }
-        List<IssueFile> list = fileService.queryFilesByIssueId(issueId);
-        request.getSession().setAttribute(KEY.ISSUE_ID, issueId);
-        JSONObject json = new JSONObject();
-        json.put("issue", issues.get(0));
-        json.put("list", list);
-        return ResultUtil.success(json);
-    }
+	@ResponseBody
+	@RequestMapping(value = "/queryIssueFiles")
+	public Object queryIssueFiles(@RequestParam(value = "issueId", required = true) String issueId,
+			HttpServletRequest request) {
+		String user = request.getSession().getAttribute(KEY.USER_NAME).toString();
+		IssueQueryCondition con = new IssueQueryCondition();
+		con.setIssueId(issueId);
+		con.setUser(user);
+		List<Issue> issues = issueService.queryIssue(con);
+		if (issues.isEmpty()) {
+			return ResultUtil.errorWithMsg("查询话题文件失败");
+		}
+		List<IssueFile> list = fileService.queryFilesByIssueId(issueId);
+		request.getSession().setAttribute(KEY.ISSUE_ID, issueId);
+		JSONObject json = new JSONObject();
+		json.put("issue", issues.get(0));
+		json.put("list", list);
+		return ResultUtil.success(json);
+	}
 
-    @ResponseBody
-    @RequestMapping(value = "/deleteFileById")
-    public Object deleteFileById(@RequestParam(value = "fileid", required = true) String fileId,
-            HttpServletRequest request) {
-        String issueId = issueService.getCurrentIssueId(request);
-        if (StringUtils.isEmpty(issueId)) {
-            return ResultUtil.errorWithMsg("获取当前话题失败,请重新进入话题");
-        }
-        int i = fileService.deleteById(fileId);
-        if (i > 0) {
-            return ResultUtil.success("删除成功");
-        }
-        return ResultUtil.errorWithMsg("删除失败");
-    }
+	@ResponseBody
+	@RequestMapping(value = "/deleteFileById")
+	public Object deleteFileById(@RequestParam(value = "fileid", required = true) String fileId,
+			HttpServletRequest request) {
+		String issueId = issueService.getCurrentIssueId(request);
+		if (StringUtils.isEmpty(issueId)) {
+			return ResultUtil.errorWithMsg("获取当前话题失败,请重新进入话题");
+		}
+		int i = fileService.deleteById(fileId);
+		if (i > 0) {
+			return ResultUtil.success("删除成功");
+		}
+		return ResultUtil.errorWithMsg("删除失败");
+	}
 
-    @ResponseBody
-    @RequestMapping("/getColumnTitle")
-    public Object getColumnTime(@RequestParam(value = "file", required = true) MultipartFile file) {
-        if (file.isEmpty()) {
-            return ResultUtil.errorWithMsg("文件是空的");
-        }
-        try {
-            List<String[]> list = ExcelUtil.read(file.getOriginalFilename(), file.getInputStream(), 0);
-            return ResultUtil.success(list.get(0));
-        } catch (Exception e) {
-            logger.warn("read column title fail" + e.toString());
-        }
-        return ResultUtil.errorWithMsg("获取列表题失败");
-    }
+	@ResponseBody
+	@RequestMapping("/getColumnTitle")
+	public Object getColumnTime(@RequestParam(value = "file", required = true) MultipartFile file) {
+		if (file.isEmpty()) {
+			return ResultUtil.errorWithMsg("文件是空的");
+		}
+		try {
+			List<String[]> list = ExcelUtil.read(file.getOriginalFilename(), file.getInputStream(), 0);
+			return ResultUtil.success(list.get(0));
+		} catch (Exception e) {
+			logger.warn("read column title fail" + e.toString());
+		}
+		return ResultUtil.errorWithMsg("获取列表题失败");
+	}
 }
