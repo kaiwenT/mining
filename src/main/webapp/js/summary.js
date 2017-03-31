541c1fd4abb38ced4e1d33954413347be0b55859
gaoyan <347259020@qq.com>
2017-03-31 19:24:23 +0800

    update UI

--- src/main/webapp/js/summary.js
+++ src/main/webapp/js/summary.js
@@ -5,68 +5,26 @@
 //4.1汇总
 
 function fileSummary(){
-	var value = $(".summary_time input[name = 'timeradio']:checked").val();
-	var end = new Date;
-	var strEnd = "" + end.getFullYear() + "-";
-	strEnd += (end.getMonth()+1) + "-";
-	strEnd += end.getDate();
-	//console.log(strEnd);
-	/*var  month = enddate.getMonth()+1;
-	if(month.toString().length == 1){
-		month = '0'+ month;
-	}*/
+	var fileId = $(".summary_up input:checked").attr("class");
+	console.log(fileId);
 	
-	if( value == "1" ){
-		$(".summary_cont").css('display','block');
-		var start = new Date( end.setDate(end.getDate()-7));
-		var strStart = "" + start.getFullYear() + "-";
-		strStart += (start.getMonth()+1) + "-";
-		strStart += start.getDate();
-		console.log(strStart);
-	}else if( value == "2" ){
-		$(".summary_cont").css('display','block');
-		var start = new Date( end.setMonth(end.getMonth()-1));
-		var strStart = "" + start.getFullYear() + "-";
-		strStart += (start.getMonth()+1) + "-";
-		strStart += start.getDate();
-		//console.log(strStart);
-	}else if( value == "3" ){
-		$(".summary_cont").css('display','block');
-		var start = new Date( end.setMonth(end.getMonth()-3));
-		var strStart = "" + start.getFullYear() + "-";
-		strStart += (start.getMonth()+1) + "-";
-		strStart += start.getDate();
-		//console.log(strStart);
-	}else if( value == "4"){
-		$(".summary_cont").css('display','block');
-        var strEnd = $(".lol_end").val();
-        var strStart = $(".lol_begin").val();
-		console.log(strEnd);
-		console.log(strStart);
-    }else{
-			
-	}
 	//console.log(value);
 		
 	$.ajax({
 		type:"post",
-		//url:"http://1v5002132k.iask.in:13020/xinheng/issue/miningByTime",
 		url:"/issue/miningByTime",
 		data:{
-			startTime: strStart,
-			endTime: strEnd
+			fileId:fileId
 		} ,
 		dataType:"json",
 		success:function(msg){
 			//console.log(msg);
 			if(msg.status == "OK"){
 				var items = msg.result;
-				$('.summary_tab table').html('');
+//				$('.summary_tab table').html('');
 				$.each(items,function(i,item){
-					/*rows= '<tr><td width="280"><div class="tab_tit"><input type="checkbox" class="choose" />'+'数量：'+item[0]+'</div></td><td width="120" align="center"><div class="tab_from">'+'网站：'+item[1]+'</div></td><td width="230" align="center">'+'标题：'+item[2]+'</td><td width="80" align="center">'+'时间：'+item[3]+'</td></tr>'
-					$('.summary_tab table').append( rows );*/
-					rows = '<tr><td height="32" align="center"><input type="checkbox" /></td><td height="32" align="center">'+item[1]+'</td><td height="32" align="center">'+高岩+'</td><td height="32" align="center">'+item[2]+'</td></tr>'
-					$('.summary_up table').append( rows );
+					rows='<tr><td><input type="checkbox" class="choose" /><a href="#">'+标题：12岁女学生上吊死亡+'</a></td><td width="120" align="center">'+网站：新华网+'</td><td width="230" align="center">'+发布时间：2016-12-17 12:00:00+'</td><td width="80" align="center">'+数量：260+'</td></tr>'
+					$('.summary_tab table').append( rows );
 				})
 			}else{
 				alert("fail");
@@ -131,11 +89,9 @@ function fileSearch(){
                 //alert("success") ;
                 var items = msg.result;
                 //console.log(msg)
-                $('.summary_up table').html('');
+                $('.summary_up table tr:not(:first)').html('');
                 $.each(items,function(i,item){
-                    /*rows= '<tr><td width="280"><div class="tab_tit"><input type="checkbox" class="choose" />'+'数量：'+item[0]+'</div></td><td width="120" align="center"><div class="tab_from">'+'网站：'+item[1]+'</div></td><td width="230" align="center">'+'标题：'+item[2]+'</td><td width="80" align="center">'+'时间：'+item[3]+'</td></tr>'
-                    $('.summary_tab table').append( rows );*/
-                    rows = '<tr><td height="32" align="center"><input type="checkbox" /></td><td height="32" align="center">'+item.fileName+'</td><td height="32" align="center">'+item.creator+'</td><td height="32" align="center">'+new Date(item.uploadTime.time).format('yyyy-MM-dd hh:mm:ss')+'</td></tr>'
+                    rows = '<tr><td height="32" align="center"><input type="checkbox" class="'+item.fileId+'" /></td><td height="32" align="center">'+item.fileName+'</td><td height="32" align="center">'+item.creator+'</td><td height="32" align="center">'+new Date(item.uploadTime.time).format('yyyy-MM-dd hh:mm:ss')+'</td></tr>'
                     $('.summary_up table').append( rows );
                 })
             }else{
