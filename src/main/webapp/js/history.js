c03f59d168103cf6798241e6f6f396b2849a9f1a
gaoyan <347259020@qq.com>
2017-04-02 02:45:17 +0800

    fix history bugs

--- src/main/webapp/js/history.js
+++ src/main/webapp/js/history.js
@@ -2,67 +2,81 @@
  * Created by Administrator on 2016/12/18.
  */
 
-$(document).ready(function(){
-    $.ajax({
-        type:"post",
-        url:"/result/queryResultList",
-        success:function(msg){
-            var items = msg.result;
-            $('.summary_up table tr:not(:first)').html('');
-            $.each(items,function(i, item) {
-                        rows = '<tr><td height="32" align="center"><span id="'+item.rid+'" onclick="historyData()">'
-                                + item.comment
-                                + '</span></td><td height="32" align="center">'
-                                + item.creator
-                                + '</td><td height="32" align="center">'
-                                + new Date(item.createTime.time).format('yyyy-MM-dd hh:mm:ss')
-                                + '</td><td height="32" align="center"><img src="images/delete.png" onclick="fileDel()" /></td></tr>'
-                        $('.summary_up table').append(rows);
-                    })
-        },
-        error : function(msg){
-            alert("数据请求失败");
-        }
-    });
-});
+$(document)
+        .ready(
+                function() {
+                    $
+                            .ajax({
+                                type : "post",
+                                url : "/result/queryResultList",
+                                success : function(msg) {
+                                    var items = msg.result;
+                                    $('.summary_up table tr:not(:first)').html(
+                                            '');
+                                    $
+                                            .each(
+                                                    items,
+                                                    function(i, item) {
+                                                        rows = '<tr><td height="32" align="center"><a href="javascript:;" onclick="historyData(\''
+                                                                + item.rid
+                                                                + '\')">'
+                                                                + item.comment
+                                                                + '</a></td><td height="32" align="center">'
+                                                                + item.creator
+                                                                + '</td><td height="32" align="center">'
+                                                                + new Date(
+                                                                        item.createTime.time)
+                                                                        .format('yyyy-MM-dd hh:mm:ss')
+                                                                + '</td><td height="32" align="center"><img src="images/delete.png" onclick="fileDel()" /></td></tr>'
+                                                        $('.summary_up table')
+                                                                .append(rows);
+                                                    })
+                                },
+                                error : function(msg) {
+                                    alert("数据请求失败");
+                                }
+                            });
+                });
 
-function historyData(){
-	$(".summary_up tr span").click(function(){
-		var historyId=$(this).attr("id");
-		$.ajax({
-		type:"post",
-		url:"/result/getCountResult",
-		data:{
-		    resultId:historyId
-		},
-		dataType:"json",
-		success: function(msg){
-			console.log(msg);
-			if( msg.status == "OK"){
-				// alert("删除成功");
-				var items = msg.result;
-                $('.summary_tab table tr:not(:first)').html('');
-                $.each(items,function(i, item) {
-	                rows = '<tr><td height="32" align="center"><input type="checkbox" class="'
-	                        + i + '"/></td><td height="32" align="center"><a href="'
-	                        + item[1]
-	                        + '">'
-	                        + item[2]
-	                        + '</a></td><td height="32" align="center">'
-	                        + item[3]
-	                        + '</td><td height="32" align="center">'
-	                        + item[0] + '</td></tr>'
-	                $('.summary_tab table').append(rows);
-	            })
-			}else{
-				alert(msg.result);
-			}
-		},
-		error: function(){
-			alert("请求失败");
-		}
-	})	
-	})
+function historyData(rid) {
+    $.ajax({
+                type : "post",
+                url : "/result/getCountResult",
+                data : {
+                    resultId : rid
+                },
+                dataType : "json",
+                success : function(msg) {
+                    console.log(msg);
+                    if (msg.status == "OK") {
+                        // alert("删除成功");
+                        var items = msg.result;
+                        $('.summary_tab table tr:not(:first)').html('');
+                        $
+                                .each(
+                                        items,
+                                        function(i, item) {
+                                            rows = '<tr><td height="32" align="center"><input type="checkbox" class="'
+                                                    + i
+                                                    + '"/></td><td height="32" align="center"><a href="'
+                                                    + item[1]
+                                                    + '">'
+                                                    + item[2]
+                                                    + '</a></td><td height="32" align="center">'
+                                                    + item[3]
+                                                    + '</td><td height="32" align="center">'
+                                                    + item[0] + '</td></tr>'
+                                            $('.summary_tab table')
+                                                    .append(rows);
+                                        })
+                    } else {
+                        alert(msg.result);
+                    }
+                },
+                error : function() {
+                    alert("请求失败");
+                }
+            })
 }
 
 /* 合并 */
